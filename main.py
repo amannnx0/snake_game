e6f714bf30de1146b01ed210431f4e85358ce5c1
Aman <meamanx7@gmail.com>
2025-03-27 15:51:05 +0530

    created snake body

--- main.py
+++ main.py
@@ -1,5 +1,29 @@
-print("hello")
-print("asdf")
-a = 5
-b = 7
-print(a+b)
+from turtle import Turtle,Screen
+screen = Screen()
+screen.setup(width=600,height=600)
+screen.bgcolor("black")
+screen.title("My Snake Game")
+starting_position = [(0,0),(-20,0),(-40,0)]
+fragment = []
+for i in starting_position:
+    new_frag = Turtle("square")
+    new_frag.color("white")
+    new_frag.goto(i)
+    fragment.append(new_frag)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
