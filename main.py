92ea33dae8f864716f93c9532d99060367eda940
Aman <meamanx7@gmail.com>
2025-03-28 07:51:03 +0530

    added tracer

--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 from turtle import Turtle,Screen
 screen = Screen()
 screen.setup(width=600,height=600)
-screen.bgcolor("black")
-screen.title("My Snake Game")
+screen.bgcolor("gray")
+screen.tracer(0)
+
 starting_position = [(0,0),(-20,0),(-40,0)]
-fragment = []
-for i in starting_position:
-    new_frag = Turtle("square")
-    new_frag.color("white")
-    new_frag.goto(i)
-    fragment.append(new_frag)
+segments = []
+for position in starting_position:
+    new_seg = Turtle("square")
+    new_seg.penup()
+    new_seg.color("black")
+    new_seg.goto(position)
+    segments.append(new_seg)
+    screen.update()
 
 
-
-
-
-
-
-
+is_game_on = True
+while is_game_on:
+    for seg in segments:
+        seg.forward(20)
 
 
 
